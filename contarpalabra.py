4e6f09b4729b54d63421e824ad69213a143bd538
Sofi16044 <s.soler@alumno.um.edu.ar>
2023-04-11 09:43:22 -0300

    contarpalabra

--- contarpalabra.py
+++ contarpalabra.py
@@ -1,20 +1,18 @@
 import unittest
 def palabra(pala):
+    dicc = {}
     r = pala.split(" ")
     for d in r:
-        cantpala = r.count(d) #cuenta cantidad de veces de una palabra, no cantidad de palabras
-        if cantpala >= 2:
-            r.remove(d)
-    cant = len(r)
-    return (cant)
+        cantpala = r.count(d) #el count cuenta cantidad de veces de una palabra, no cantidad de palabras
+        key, value = d, cantpala
+        dicc[key] = value
 
-
+    return (dicc)
+# la funcion cuenta cantidad de palabras, si hay 2 repetidas las cuenta como 1
 
     
 if __name__ == "__main__":
     unittest.main()
-
-
     
 
 
